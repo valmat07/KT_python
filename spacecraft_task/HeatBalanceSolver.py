b214b68bce5365ac0ec391a0e5745890bb366af6
unknown <valmat07@yandex.ru>
2021-03-13 19:37:12 +0300

    add calc areas, stationary solver for heat balance equation

--- spacecraft_task/HeatBalanceSolver.py
+++ spacecraft_task/HeatBalanceSolver.py
@@ -2,6 +2,8 @@ import pandas as pd
 from scipy.integrate import odeint
 import numpy as np
 from py_expression_eval import Parser
+from scipy.optimize import fsolve
+
 class HeatBalanceSolver():
     def __init__(self, parametrs_file_name, surfaces_area, area_btw_surfaces):
         super(HeatBalanceSolver, self).__init__()
@@ -9,23 +11,26 @@ class HeatBalanceSolver():
         self.surfaces_area = surfaces_area
         self.area_btw_surfaces = area_btw_surfaces
         self.c_0 = 5.67
+        self.amount_elemnts = len(self.area_btw_surfaces)
+
         self.parse_parametrs()
-        self.A = 1
+        self.A = 100
 
     def _equation(self, T, t):
         dTdt = []
-        for i in range(len(self.area_btw_surfaces)):
+        for i in range(self.amount_elemnts):
             tmp_eq = 0
-            for j in range(len(self.area_btw_surfaces)):
+            for j in range(self.amount_elemnts):
                 k = self.lambdas[i, j] * self.area_btw_surfaces[i, j]
                 tmp_eq += k * (T[j] - T[i])
             q_e = -self.epsilon[i] * self.surfaces_area[i] * self.c_0 * ((T[i] / 100) ** 4)
 
             if i == 1:
                 tmp_eq += q_e + self.A*(20 + 3 * np.cos(t/4))
-                
+
             dTdt.append(tmp_eq / self.c[i])
         return dTdt
+
     def parse_parametrs(self):
         parametrs_df = pd.read_csv(self.parametrs_file_name)
         self.epsilon = np.array(parametrs_df['eps'])
@@ -37,12 +42,19 @@ class HeatBalanceSolver():
         for i in range(self.lambdas.shape[0] - 1):
             self.lambdas[i] = np.array(parametrs_df['lambdas_{}_x'.format(i + 1)])
         
+        for i in range(1, self.amount_elemnts):
+            for j in range(i, self.amount_elemnts):
+                self.lambdas[j, i] = self.lambdas[i, j]
+
         for value in parametrs_df['q_r']:
             self.q_r.append(value)
 
 
+    def get_stationary_solution(self):
+        return fsolve(self._equation, np.zeros(self.amount_elemnts), args=(0.0))
+
     def solve(self, t0, t1):
         t = np.linspace(t0, t1)
-        init_cond =  np.ones(5)
+        init_cond =  np.zeros(5)#self.get_stationary_solution()
         sol = odeint(self._equation, init_cond, t)
         return sol
--- spacecraft_task/ModelParser.py
+++ spacecraft_task/ModelParser.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.linalg import norm
 class ModelParser():
     def __init__(self, file_name=None):
         super(ModelParser, self).__init__()
@@ -35,9 +36,9 @@ class ModelParser():
                     for idx in split[1:]:
                         face.append(vertexList[int(idx) - 1])
                     one_part_list.append(face)
-            #added last part of model
+            #add last part of model
             parts_list.append(one_part_list)
-        
+
         normals_list = []
         for part in parts_list:
             normals_list_part = []
@@ -46,7 +47,45 @@ class ModelParser():
                 b = np.array(surface[1]) - np.array(surface[0])
                 normals_list_part.append((-np.cross(a, b)).tolist())
             normals_list.append(normals_list_part)
+
         return parts_list, normals_list
+   
+    def _triangle_area(self, a, b, c) :
+        a, b, c = np.array(a), np.array(b), np.array(c)
+        return 0.5 * norm(np.cross(b - a, c - a))
     
-    def getAreas(self):
-        raise NotImplementedError('not implemented yet')
+    def _poly_area(self, x, y):
+        x, y = np.array(x), np.array(y)
+        return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+    def getAreas(self, parts_list):
+        surfaces_area = np.zeros(len(parts_list))
+        surfaces_area_btw_parts = np.zeros((len(parts_list), len(parts_list)))
+
+        for i, part in enumerate(parts_list):
+            for surface in part:
+                surfaces_area[i] += self._triangle_area(*surface)
+            
+            #now if we assume that the intersection is a 2D plane
+            #area can be calculated for a polygon
+            for j, second_part in enumerate(parts_list[i + 1:]):
+                polygon_vert_x, polygon_vert_z = [], []
+                tmp_verts = []
+                for surface in part:
+                    for second_surface in second_part:
+                        for vert in surface:
+                            for second_vert in second_surface:
+                                
+                                if (vert[1] == second_vert[1])  and (not vert in tmp_verts):
+                                    polygon_vert_x.append(vert[0])
+                                    polygon_vert_z.append(vert[2])
+                                    tmp_verts.append(vert)
+
+                surfaces_area_btw_parts[i, i + j + 1] = self._poly_area(polygon_vert_x, polygon_vert_z)
+                print(i, j, len(polygon_vert_x), self._poly_area(polygon_vert_x, polygon_vert_z))
+
+        for i in range(1, len(parts_list)):
+            for j in range(i, len(parts_list)):
+                surfaces_area_btw_parts[j, i] = surfaces_area_btw_parts[i, j]
+
+        return surfaces_area, surfaces_area_btw_parts

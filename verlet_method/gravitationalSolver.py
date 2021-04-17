67c4eef16ab4aeafa2be650cc1546dcbff0ce9e5
valmat07 <valmat07@yandex.ru>
2021-04-17 17:12:18 +0300

    add multiprocessing function

--- verlet_method/gravitationalSolver.py
+++ verlet_method/gravitationalSolver.py
@@ -8,30 +8,52 @@ from concurrent import futures
 import multiprocessing
 from functools import partial
 import cython_solve
+from multiprocessing import Process, Queue
+
 dt = 1e0
 gravi_const = 6.6742 * 1e-11 * (3600 ** 2)
-def _calc_accelerations_mp(x_pos, y_pos, weights, i):
+global weights_mp
+def _calc_accelerations_mp(x_pos, y_pos, amount_elements, i):
     v_equation_x, v_equation_y = 0, 0
-    for j in range(len(x_pos)):
+    global weights_mp
+    for j in range(amount_elements):
         if j == i:
             continue
         if x_pos[j] != x_pos[i]:
-            v_equation_x +=  gravi_const * weights[j] * (x_pos[j] - x_pos[i])/np.sqrt((x_pos[j] - x_pos[i]) ** 2 + (y_pos[j] - y_pos[i]) ** 2) ** 3
+            v_equation_x +=  gravi_const * weights_mp[j] * (x_pos[j] - x_pos[i])/np.sqrt((x_pos[j] - x_pos[i]) ** 2 + (y_pos[j] - y_pos[i]) ** 2) ** 3
         if y_pos[j] != y_pos[i]:
-            v_equation_y +=  gravi_const * weights[j] * (y_pos[j] - y_pos[i])/np.sqrt((x_pos[j] - x_pos[i]) ** 2 + (y_pos[j] - y_pos[i]) ** 2) ** 3
+            v_equation_y +=  gravi_const * weights_mp[j] * (y_pos[j] - y_pos[i])/np.sqrt((x_pos[j] - x_pos[i]) ** 2 + (y_pos[j] - y_pos[i]) ** 2) ** 3
+    
     return v_equation_x, v_equation_y
 
-def _calc_speeds_verlet_mp(x_pos, y_pos, speed_x, speed_y, acceleration_x, acceleration_y, weights, i):
-    acceleration_x_next, acceleration_y_next = _calc_accelerations_mp(x_pos, y_pos, weights, i)
-    tmp1 = speed_x[i]  + 0.5 * (acceleration_x_next + acceleration_x[i]) * dt
-    tmp2 = speed_y[i]  + 0.5 * (acceleration_y_next + acceleration_y[i]) * dt
-    return tmp1, tmp2
+def _calc_pos_prosess(N, amount_elements, init_position, pos_queue, speed_queue, return_pos_queue):
+    x_pos = np.zeros((N, amount_elements))
+    y_pos = np.zeros((N, amount_elements))
+    x_pos[0], y_pos[0] = init_position[:, 0], init_position[:, 1]
+    for n in range(0, N - 1):
+        acceleration_x, acceleration_y = np.zeros(amount_elements), np.zeros(amount_elements)
+        for i in range(amount_elements):
+            acceleration_x[i], acceleration_y[i] = _calc_accelerations_mp(x_pos[n], y_pos[n], amount_elements, i)
+        curr_speed_x, curr_speed_y = speed_queue.get()
+        for i in range(amount_elements):
+            x_pos[n + 1, i] = x_pos[n, i] + curr_speed_x[i] * dt + acceleration_x[i] * (dt ** 2) / 2
+            y_pos[n + 1, i] = y_pos[n, i] + curr_speed_y[i] * dt + acceleration_y[i] * (dt ** 2) / 2
+        pos_queue.put([x_pos[n + 1], y_pos[n + 1], acceleration_x, acceleration_y])
+    return_pos_queue.put([x_pos, y_pos])
 
-def _calc_positions_verlet_mp(x_pos, y_pos, speed_x, speed_y, weights, i):
-    acceleration_x, acceleration_y = _calc_accelerations_mp(x_pos, y_pos, weights, i)
-    tmp1 = x_pos[i] + speed_x[i] * dt + acceleration_x * (dt ** 2) / 2
-    tmp2 = y_pos[i] + speed_y[i] * dt + acceleration_y * (dt ** 2) / 2
-    return tmp1, tmp2, acceleration_x, acceleration_y
+def _clac_speed_prosess(N, amount_elements, init_speed, pos_queue, speed_queue, return_speed_queue):
+    speed_x = np.zeros((N, amount_elements))
+    speed_y = np.zeros((N, amount_elements))
+    speed_x[0], speed_y[0] = np.zeros(amount_elements), init_speed
+    speed_queue.put([speed_x[0], speed_y[0]])
+    for n in range(0, N - 1):
+        curr_x_pos, curr_y_pos, acceleration_x, acceleration_y = pos_queue.get()
+        for i in range(amount_elements):
+            acceleration_x_next, acceleration_y_next = _calc_accelerations_mp(curr_x_pos, curr_y_pos, amount_elements, i)
+            speed_x[n + 1, i] = speed_x[n, i]  + 0.5 * (acceleration_x_next + acceleration_x[i]) * dt
+            speed_y[n + 1, i] = speed_y[n, i]  + 0.5 * (acceleration_y_next + acceleration_y[i]) * dt
+        speed_queue.put([speed_x[n + 1], speed_y[n + 1]])
+    return_speed_queue.put([speed_x, speed_y])
 
 class GravitationalSolver():
     def __init__(self, weights, init_speed, init_position):
@@ -39,6 +61,8 @@ class GravitationalSolver():
         self.amount_elements = len(weights)
         self.gravi_const = 6.6742 * 1e-11 * (3600 ** 2) #metrs^3 hours^-2 kilo^-1
         self.weights = weights
+        global weights_mp
+        weights_mp = weights
         self.init_speed = init_speed
         self.init_position = init_position
 
@@ -105,8 +129,6 @@ class GravitationalSolver():
 
         return np.concatenate((x_pos, y_pos, speed_x, speed_y), axis=-1)
 
-
-
     def solve_verlet_threading(self, max_time, dt):
         def _calc_positions_verlet(x_pos, y_pos, speed_x, speed_y, i):
             acceleration_x, acceleration_y = self._calc_accelerations(x_pos, y_pos, i)
@@ -126,12 +148,12 @@ class GravitationalSolver():
         speed_x = np.zeros((N, self.amount_elements))
         speed_y = np.zeros((N, self.amount_elements))
 
-
         x_pos[0], y_pos[0] = self.init_position[:, 0], self.init_position[:, 1]
         speed_x[0], speed_y[0] =  np.zeros(self.amount_elements), self.init_speed
         
         for n in range(N - 1):
             acceleration_x, acceleration_y = np.zeros(self.amount_elements), np.zeros(self.amount_elements)
+    
             with ThreadPoolExecutor(max_workers=self.amount_elements) as executor:
                 jobs = []
                 for i in range(self.amount_elements):
@@ -152,36 +174,28 @@ class GravitationalSolver():
                     result_done = job.result()
                     i = result_done[-1]
                     speed_x[n + 1, i], speed_y[n + 1, i] = result_done[0], result_done[1]
-       
+
+            
+
         return np.concatenate((x_pos, y_pos, speed_x, speed_y), axis=-1)
     
     def solve_verlet_multitasking(self, max_time, dt):
         N = int(max_time / dt)
-        x_pos = np.zeros((N, self.amount_elements))
-        y_pos = np.zeros((N, self.amount_elements))
-        speed_x = np.zeros((N, self.amount_elements))
-        speed_y = np.zeros((N, self.amount_elements))
+        pos_queue, speed_queue = Queue(), Queue()
+        return_pos_queue, return_speed_queue = Queue(), Queue()
+        #create processes. First process for calc position, second for speeds
+        pos_prosess = Process(target=_calc_pos_prosess, args=(N, self.amount_elements, self.init_position, pos_queue, speed_queue, return_pos_queue))
+        speed_prosess = Process(target=_clac_speed_prosess, args=(N, self.amount_elements, self.init_speed, pos_queue, speed_queue, return_speed_queue))
 
-        x_pos[0], y_pos[0] = self.init_position[:, 0], self.init_position[:, 1]
-        speed_x[0], speed_y[0] =  np.zeros(self.amount_elements), self.init_speed
-        tmp_lst1 = [(np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), i) for i in range(self.amount_elements)]
-        tmp_lst2 = [(np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), np.zeros(self.amount_elements), i) for i in range(self.amount_elements)]
-        for n in range(0, N - 1):
-           # acceleration_x, acceleration_y = np.zeros(self.amount_elements), np.zeros(self.amount_elements)
+        pos_prosess.start()
+        speed_prosess.start()
 
-            #tmp_lst = [(x_pos[n], y_pos[n], speed_x[n], speed_y[n], self.weights, i) for i in range(self.amount_elements)]
-            
-            
-            with multiprocessing.Pool(processes=self.amount_elements) as pool:
-                results = np.array(pool.starmap(_calc_positions_verlet_mp, tmp_lst1))
-                x_pos[n + 1], y_pos[n + 1] = results[:, 0], results[:, 1]
-                acceleration_x, acceleration_y  = results[:, 2], results[:, 3] 
+        pos_prosess.join()
+        x_pos, y_pos = return_pos_queue.get()
 
-            #tmp_lst = [(x_pos[n + 1], y_pos[n + 1], speed_x[n], speed_y[n], acceleration_x, acceleration_y, self.weights, i) for i in range(self.amount_elements)]
-            
-            with multiprocessing.Pool(processes=self.amount_elements) as pool:
-                results = np.array(pool.starmap(_calc_speeds_verlet_mp, tmp_lst2))
-                speed_x[n + 1], speed_y[n + 1] = results[:, 0], results[:, 1]
+        speed_prosess.join()
+        speed_x, speed_y = return_speed_queue.get()
+
         return np.concatenate((x_pos, y_pos, speed_x, speed_y), axis=-1)
     
     def solve_verlet_cython(self, max_time, dt):

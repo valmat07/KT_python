5745899f01f662fe3ce4625c5ea38de90a80b999
valmat07 <valmat07@yandex.ru>
2021-06-01 22:32:47 +0300

    add test functions for both methods

--- MKE/PlotSolutions.py
+++ MKE/PlotSolutions.py
@@ -9,7 +9,7 @@ class PlotSolutions():
     def __init__(self):
         pass
     
-    def create_plot(self, mesh, u, h, figsize = None):
+    def create_plot(self, mesh, u, h, func_name, figsize = None):
         n = mesh.num_vertices()
         d = mesh.geometry().dim()
         mesh_coordinates = mesh.coordinates().reshape((n, d))
@@ -20,6 +20,7 @@ class PlotSolutions():
 
         approx_sol = np.asarray([u(cell.midpoint()) for cell in cells(mesh)])
         exact_sol = np.asarray([h(cell.midpoint()) for cell in cells(mesh)])
+        plt.suptitle(func_name, fontsize=20)
         plt.subplot(121)
         plt.title('Approx solution')
         plt.tripcolor(triangulation, facecolors=approx_sol, edgecolors='k')
@@ -28,10 +29,11 @@ class PlotSolutions():
         plt.title('Exact solution')
         plt.tripcolor(triangulation, facecolors=exact_sol, edgecolors='k')
 
-    def plot_error(self, error_L2, error_C, figsize=None):
+    def plot_error(self, error_L2, error_C, func_name, figsize=None):
 
         if figsize is not None:
             fig = plt.figure(figsize=figsize)
+        plt.suptitle(func_name)
         plt.subplot(121)
         plt.title('Error L2')
         plt.xlabel('Time steps')
@@ -56,18 +58,24 @@ class PlotSolutions():
         
         self.ax.set_aspect('equal')
         img = self.ax.tripcolor(triangulation, facecolors=self.zface_u[i] / self.max_u, edgecolors='k', vmin=0, vmax=1)
+        self.ax.set_title('Numerical solution')
+
 
         self.ax1.set_aspect('equal')
         img1 = self.ax1.tripcolor(triangulation, facecolors=self.zface_u_D[i] / self.max_u_D, edgecolors='k', vmin=0, vmax=1)
+        self.ax1.set_title('Exact solution')
+
         
         return img, img1
 
-    def create_gif(self, zface_u, zface_u_D, mesh):
+    def create_gif(self, zface_u, zface_u_D, mesh, func_name):
         self.zface_u = zface_u
         self.zface_u_D = zface_u_D
         self.mesh = mesh
-        self.fig = plt.figure(figsize=(15, 15))
+        plt.style.use('classic')
 
+        self.fig = plt.figure(figsize=(7, 7))
+        plt.suptitle(func_name)
         self.max_u = zface_u.max()
         self.max_u_D = zface_u_D.max()
 
@@ -79,5 +87,6 @@ class PlotSolutions():
         anim = animation.FuncAnimation(self.fig, 
                                       self._animate, 
                                       frames=len(zface_u_D),
-                                      repeat = True)
+                                      repeat = True
+                                      )
         return anim
